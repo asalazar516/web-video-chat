ec453f4d6df2074771feacb0c59c2e6b3c69488c
asalazar516 <asalazar51694@gmail.com>
2020-09-28 22:13:34 -0700

    Changing myPeer port from local to HTTPS

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     // path: '/peerjs',
     host: '/',
-    port: '3001'
+    port: '443'
 })
 
 const myVideo = document.createElement('video')
